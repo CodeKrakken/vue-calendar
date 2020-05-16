728d0a8b25ee19e571ca6df25815e2a7d9502d38
CodeKrakken <newholm1@hotmail.com>
2020-05-16 15:17:13 +0100

    Move methods inside app object

--- main.js
+++ main.js
@@ -2,20 +2,8 @@ var eventBus = new Vue()
 
 Vue.config.devtools = true
 
-function format(event) {
-  var startMinutes = ((event.start + 540) % 60)
-  var endMinutes = ((event.end + 540) % 60)
-  if (startMinutes < 10) { startMinutes = startMinutes + "0" }
-  if (endMinutes < 10) { endMinutes = endMinutes + "0" }
-  event.start = Math.floor((event.start + 540) / 60) + ":" + startMinutes
-  event.end = Math.floor((event.end + 540) / 60) + ":" + endMinutes
-}
-
 function renderDay(events) {
-  app.events = events.sort((a, b) => a.start - b.start)
-  events.forEach(event =>
-    format(event)
-  ) 
+  app.renderDay(events)
 }
 
 Vue.component('calendar-day', {
@@ -48,6 +36,22 @@ var app = new Vue({
     events: [],
     eventStarts: []
   },
+  methods: {
+    renderDay(events) {
+      app.events = events.sort((a, b) => a.start - b.start)
+      events.forEach(event =>
+        this.format(event)
+      ) 
+    },
+    format(event) {
+      var startMinutes = ((event.start + 540) % 60)
+      var endMinutes = ((event.end + 540) % 60)
+      if (startMinutes < 10) { startMinutes = startMinutes + "0" }
+      if (endMinutes < 10) { endMinutes = endMinutes + "0" }
+      event.start = Math.floor((event.start + 540) / 60) + ":" + startMinutes
+      event.end = Math.floor((event.end + 540) / 60) + ":" + endMinutes
+    }
+  }
 })
 
 window.renderDay([{start: 30, end: 120}, {start: 300, end: 330}, {start: 290, end: 330}])
