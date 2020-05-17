f88ec04651c9602b7484fba1f4336a76ec6b31af
CodeKrakken <newholm1@hotmail.com>
2020-05-17 09:36:23 +0100

    Remove extraneous files

--- main2.js
+++
@@ -1,9 +0,0 @@
-<ul>
-      <div v-for="event in events">
-        <div
-          :class="{ clash: event.isClash === true }"
-        >
-          <li>{{ event.start }} - {{ event.end }}</li>
-        </div>
-      </div>
-    </ul>
